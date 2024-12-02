479c9a49d0098ca362936a81e9db59cbfccd51b8
arthurLacombe <arthurlacombe44@gmail.com>
2024-12-02 09:47:23 +0100

    :sparkles: | add database creation and migration

--- database/database.go
+++ database/database.go
@@ -1,1 +1,31 @@
 package database
+
+import (
+	"log"
+
+	"vet-clinic-api/database/dbmodel"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDatabase() {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("animal_api.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	DB.AutoMigrate(&dbmodel.AnimalSound{})
+	DB.AutoMigrate(&dbmodel.AgeEntry{})
+	log.Println("Database connected and migrated")
+}
+
+func Migrate(db *gorm.DB) {
+	db.AutoMigrate(
+		&dbmodel.AgeEntry{},
+		&dbmodel.AnimalSound{},
+	)
+	log.Println("Database migrated successfully")
+}
